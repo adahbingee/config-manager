d4108407c634c400d8aef0ab7f4aba0f31320984
adahbingee <adahbingee@gmail.com>
2016-10-12 20:52:08 +0800

    add config macro file

--- src/config/manager/ConfigManager.cpp
+++ src/config/manager/ConfigManager.cpp
@@ -6,6 +6,7 @@
 #include "ConfigManagerRead.inc"
 #include "ConfigManagerPrint.inc"
 #include "ConfigManagerWrite.inc"
+#include "ConfigManagerMacro.inc"
 
 using namespace std;
 
@@ -17,44 +18,17 @@ void ConfigManager::read(const char *fileName) {
 	}
 
 	while( getline(file, line) ){
-		CFG_READ(CFG_0);
-		CFG_READ(CFG_1);
-		CFG_READ(CFG_2);
-		CFG_READ(CFG_3);
-		CFG_READ(CFG_4);
-		CFG_READ(CFG_5);
-		CFG_READ(CFG_6);
-		CFG_READ(CFG_7);
-		CFG_READ(CFG_8);
-		CFG_READ(CFG_9);
+		CFG_READ_MACRO
 	}
 	file.close();
 }
 
 void ConfigManager::write(const char *fileName) {
 	writeFile = fopen(fileName, "w");
-	CFG_WRITE(CFG_0);
-	CFG_WRITE(CFG_1);
-	CFG_WRITE(CFG_2);
-	CFG_WRITE(CFG_3);
-	CFG_WRITE(CFG_4);
-	CFG_WRITE(CFG_5);
-	CFG_WRITE(CFG_6);
-	CFG_WRITE(CFG_7);
-	CFG_WRITE(CFG_8);
-	CFG_WRITE(CFG_9);
+	CFG_WRITE_MACRO
 	fclose( writeFile );
 }
 
 void ConfigManager::print() {
-	CFG_PRINT(CFG_0);
-	CFG_PRINT(CFG_1);
-	CFG_PRINT(CFG_2);
-	CFG_PRINT(CFG_3);
-	CFG_PRINT(CFG_4);
-	CFG_PRINT(CFG_5);
-	CFG_PRINT(CFG_6);
-	CFG_PRINT(CFG_7);
-	CFG_PRINT(CFG_8);
-	CFG_PRINT(CFG_9);
+	CFG_PRINT_MACRO
 }
