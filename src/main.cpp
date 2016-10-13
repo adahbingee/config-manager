781310077e6113d6543311567500c28b94935ec8
adahbingee <adahbingee@gmail.com>
2016-10-13 20:59:55 +0800

    add argv check

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
+#include <stdio.h>
 #include "config/manager/ConfigManager.h"
 
 int main(int argc, char *argv[]) {
+	if (argc < 3) {
+		printf("exe [input config] [output config]\n");
+		return 0;
+	}
 	const char *inFileName  = argv[1];
 	const char *outFileName = argv[2];
 	ConfigManager::read(inFileName);
